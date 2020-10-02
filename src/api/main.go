89ad5b9eb6654a8615cc04138622ebbd97466c8a
Xfin <ito.kazuhiro.gn@gmail.com>
2020-10-02 17:07:59 +0900

    test01

--- src/api/main.go
+++ src/api/main.go
@@ -1,42 +1,21 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm/dialects/mysql"
+	"fmt"
+	"net/http"
+
+	mux "github.com/gorilla/mux"
 )
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hello world!")
+}
+
 func main() {
-	DBMigrate(DBConnect())
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "成功",
-		})
-	})
-	r.Run(":8080")
-}
 
-type User struct {
-	gorm.Model
-	NickName string `json:"nickName"`
-}
+	r := mux.NewRouter()
+	r.HandleFunc("/", rootHandler)
 
-func DBMigrate(db *gorm.DB) *gorm.DB {
-	db.AutoMigrate(&User{})
-	return db
-}
-
-
-func DBConnect() *gorm.DB {
-	DBMS := "mysql"
-	USER := "root"
-	PASS := "password"
-	PROTOCOL := "tcp(mysql:3306)"
-	DBNAME := "sample"
-	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?parseTime=true"
-	db, err := gorm.Open(DBMS, CONNECT)
-	if err != nil {
-		panic(err.Error())
-	}
-	return db
+	http.Handle("/", r)
+	http.ListenAndServe(":3000", nil)
 }
